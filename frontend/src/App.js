84906d1970d1ae931a3704beffc609d06c341d6f
Paahn <paahndev@gmail.com>
2021-01-28 18:14:11 -0800

    getting data from backend, displaying on the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    const url = "http://localhost:1337/products";
+    const response = await axios.get(url);
+    if (response.data) {
+      setProducts(response.data);
+      console.log(products)
+    }
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <ul>
+        {products.map(product => (
+          <div>
+            <li key={product.id}>{product.name}</li>
+            <p>{product.description}</p>
+          </div>
+          
+        ))}
+      </ul>
     </div>
   );
 }
